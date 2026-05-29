b8813a8f31fcf75a6d2d5ec818ee1080e3429465
hiagos901 <hiago3259@gmail.com>
2026-05-29 19:40:53 -0300

    feat: add js temas

---
+++ src/js/temas.js
@@ -0,0 +1,27 @@
+function temaAzul(){
+
+  document.documentElement.style.setProperty(
+    '--verde',
+    '#00b4fcea'
+  );
+
+}
+
+function temaVerde(){
+
+  document.documentElement.style.setProperty(
+    '--verde',
+    '#2dcc7a'
+    
+  );
+
+}
+
+function temaVermelho(){
+
+  document.documentElement.style.setProperty(
+    '--verde',
+    '#ee0a0a'
+  );
+
+}
